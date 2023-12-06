254f402e21c790a72697fd4c43fae505d7ad5036
hyevvy <hyeg0@naver.com>
2023-12-06 15:21:03 +0900

    feat: 특정 주모의 상품 내역 api 연동

---
+++ src/api/search/searchAPIService.types.ts
@@ -0,0 +1,51 @@
+interface ApiResponse<T> {
+  code: number
+  message: string
+  detail?: string
+  data: T
+  failure?: string
+}
+
+export interface Page<T> {
+  content: T
+  pageable: {
+    sort: {
+      empty: boolean
+      sorted: boolean
+      unsorted: boolean
+    }
+    offset: number
+    pageSize: number
+    pageNumber: number
+    paged: boolean
+    unpaged: boolean
+  }
+  last: boolean
+  totalElements: number
+  totalPages: number
+  size: number
+  number: number
+  sort: {
+    empty: boolean
+    sorted: boolean
+    unsorted: boolean
+  }
+  first: boolean
+  numberOfElements: number
+  empty: boolean
+}
+
+export type GetProductListBySellerIdResponseData = {
+  productId: string
+  productName: string
+  totalSalesCount: number
+  productPrice: number
+  stockQuantity: number
+  reviewCount: number
+  shortsId: number
+  isActivate: boolean
+}
+
+export type GetProductListBySellerIdResponse = ApiResponse<
+  Page<GetProductListBySellerIdResponseData[]>
+>
