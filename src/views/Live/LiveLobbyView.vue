e66bc0fb863e6e393078757f4da7eca4e448b856
hyevvy <hyeg0@naver.com>
2023-12-12 16:58:09 +0900

    feat: stomp추가

--- src/views/Live/LiveLobbyView.vue
+++ src/views/Live/LiveLobbyView.vue
@@ -25,6 +25,8 @@ import CustomButton from '@/components/common/CustomButton.vue'
 import ConnectLive from '@connectlive/connectlive-web-sdk'
 import { useToast } from 'vue-toastification'
 import { startStream, finishStream } from '@/api/auction/auctionAPIService.ts'
+import Stomp from 'webstomp-client'
+import SockJS from 'sockjs-client'
 export default {
   components: { CustomButton },
   data() {
@@ -59,6 +61,44 @@ export default {
           console.error(`error occurred : ${err}`)
         })
     },
+    send() {
+      console.log('Send message:' + this.message)
+      if (this.stompClient && this.stompClient.connected) {
+        const msg = {
+          userName: this.userName,
+          content: this.message
+        }
+        this.stompClient.send('/receive', JSON.stringify(msg), {})
+      }
+    },
+    connect() {
+      console.log('here')
+      const serverURL = 'https://jeontongju-dev.shop'
+      let socket = new SockJS(`${serverURL}/chat/sub/chat/${this.auctionId}`)
+      this.stompClient = Stomp.over(socket)
+      console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`)
+      this.stompClient.connect(
+        {},
+        (frame) => {
+          // 소켓 연결 성공
+          this.connected = true
+          console.log('소켓 연결 성공', frame)
+          // 서버의 메시지 전송 endpoint를 구독합니다.
+          // 이런형태를 pub sub 구조라고 합니다.
+          this.stompClient.subscribe('/send', (res) => {
+            console.log('구독으로 받은 메시지 입니다.', res.body)
+
+            // 받은 데이터를 json으로 파싱하고 리스트에 넣어줍니다.
+            this.recvList.push(JSON.parse(res.body))
+          })
+        },
+        (error) => {
+          // 소켓 연결 실패
+          console.log('소켓 연결 실패', error)
+          this.connected = false
+        }
+      )
+    },
     async connectKaKaoLive() {
       try {
         const LIVE_KEY = import.meta.env.VITE_LIVE_KEY
@@ -69,6 +109,7 @@ export default {
           serviceId: 'ICLEXMPLPUBL',
           serviceSecret: 'ICLEXMPLPUBL0KEY:YOUR0SRVC0SECRET'
         })
+        this.connect()
       } catch (err) {
         console.error(err)
       }
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    global: {}
+  },
   plugins: [
     vue({
       template: {
