1176558769521dc188450051feb78c804a5dafa8
hyevvy <hyeg0@naver.com>
2023-12-13 17:49:05 +0900

    config: tsconfig.json 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   https: true,
-  //   hmr: {
-  //     host: 'jeontongju-dev.shop/auction-service',
-  //     protocol: 'ws'
-  //   }
-  // },
   define: {
     global: 'window',
     'process.<wbr>env': {}
