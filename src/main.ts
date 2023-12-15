1f37f23d0d2481ae1bf7b70a340a1734d0cf9b7b
hyevvy <hyeg0@naver.com>
2023-12-15 10:30:07 +0900

    chore: pinia 추가

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import './assets/main.css'
 import { loadFonts } from './plugins/webfontloader'
 import Toast from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
+import { createPinia } from 'pinia'
 
 loadFonts()
 
-createApp(App).use(router).use(Toast).mount('#app')
+createApp(App).use(router).use(Toast).use(createPinia()).mount('#app')
