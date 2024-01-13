f78983c37bc0005e4f100f0dba251a75b4b0c79b
hyevvy <hyeg0@naver.com>
2024-01-13 15:14:57 +0900

    feat: 주모의 주문 내역 페이지네이션

--- src/views/Jumo/JumoDetailOrderView.vue
+++ src/views/Jumo/JumoDetailOrderView.vue
@@ -1,29 +1,50 @@
 <template>
-  <div>
+  <div v-if="items.length > 0">
     <CustomTable :headers="header" :items="items"></CustomTable>
+    <CustomPagination
+      :on-change-page="onChangePage"
+      :request-page="requestPage"
+      :total-pages="totalPages"
+    />
+    <div v-if="isLoading">
+      <img src="../../assets/loading.gif" alt="loading" />
+    </div>
   </div>
+  <div v-else>주모의 주문 내역이 비어있어요.</div>
 </template>
 
-<script>
+<script lang="ts" scoped>
+import CustomPagination from '@/components/common/CustomPagination.vue'
 import CustomTable from '@/components/common/CustomTable.vue'
 import { getOrderListBySellerId } from '@/api/order/orderAPIService.ts'
 import { useToast } from 'vue-toastification'
+import type { GetOrderListByConsumerIdResponseData } from '@/api/order/orderAPIService.types'
 export default {
   components: {
-    CustomTable
+    CustomTable,
+    CustomPagination
   },
   methods: {
-    async getOrderListBySellerId(page, size) {
+    async onChangePage(page: number) {
+      if (0 <= page && page < this.totalPages) {
+        this.requestPage = page
+      }
+    },
+    async getOrderListBySellerId(page: number, size: number) {
       const toast = useToast()
       try {
+        this.isLoading = true
         const data = await getOrderListBySellerId(this.sellerId, page, size)
         if (data.code === 200) {
           this.items = data.data.content
+          this.totalPages = data.data.totalPages
         }
       } catch (error) {
         toast.error(`주모 주문내역들을 불러오는데 실패했어요.`, {
           timeout: 2000
         })
+      } finally {
+        this.isLoading = false
       }
     }
   },
@@ -40,12 +61,29 @@ export default {
         { text: '운송장', value: 'deliveryCode' },
         { text: '경매 여부', value: 'isAuction' }
       ],
-      items: []
+      items: [],
+      page: 0,
+      totalPages: 0,
+      requestPage: 0,
+      isLoading: false
+    } as {
+      sellerId: number
+      header: { text: string; value: string }[]
+      items: GetOrderListByConsumerIdResponseData[]
+      page: number
+      totalPages: number
+      requestPage: number
+      isLoading: boolean
     }
   },
   mounted() {
-    this.sellerId = this.$route.params.sellerId
-    this.getOrderListBySellerId(0, 10)
+    this.sellerId = Number(this.$route.params.sellerId)
+    this.getOrderListBySellerId(0, 6)
+  },
+  watch: {
+    requestPage: function (value) {
+      this.getOrderListBySellerId(value, 6)
+    }
   }
 }
 </script>
