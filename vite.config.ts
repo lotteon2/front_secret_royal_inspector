269e672358d32df535c786394fb4b6b0ec27e767
hyevvy <hyeg0@naver.com>
2024-01-19 17:32:18 +0900

    feat: manifest 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueLayouts from 'vite-plugin-vue-layouts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,7 +22,8 @@ export default defineConfig({
           }
         }
       }
-    })
+    }),
+    VueLayouts()
   ],
   resolve: {
     alias: {
